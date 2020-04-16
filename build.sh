9ed1d0c51c250964a394cb49e0712d7bfa2d5543
Claudio Canales <klaudioz@gmail.com>
2020-04-16 09:10:34 -0600

    Delete unnecessary stuff

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh -l
 
-#searchword='non-e2e'
-
-# Check if last commit has the tag $(searchword). THis grep is more strong than grep -w
-#git describe --tags $(git rev-parse --short HEAD) | grep -E "^$searchword\s|\s($searchword)\s|\s$searchword$|^$searchword$"
-
 echo "Running test $1 on cloud $2"
