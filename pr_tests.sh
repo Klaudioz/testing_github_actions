0e3c9b3d43d2b312344121b94c6924961f621e4e
Claudio Canales <klaudioz@gmail.com>
2020-04-16 15:24:48 -0600

    Fix jq parsing in pr_tests.sh

--- pr_tests.sh
+++ pr_tests.sh
@@ -7,18 +7,14 @@
 # particular labels, this function will never run.
 # -----------------------------------
 #
-# HISTORY:
-#
-# * DATE - v1.0.0  - First Creation
-#
 # ##################################################
 
-#LABELS=(echo $1 | jq '.[].name')
+# Removing spaces, slashs and newlines
+labels_json=$(echo "${1}" | sed 's/\\//g' | tr -d '\n' |tr -d ' ')
 
-for row in $(echo "$1" | jq -c '.[]'); do
+for row in $(echo "${labels_json}" | jq -c '.[]'); do
     _jq() {
      echo ${row} | jq -c ${1}
     }
-
    echo $(_jq '.name')
 done
