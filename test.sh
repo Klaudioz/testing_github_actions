d74fabe65f9b73f3af9fd1eae2f98415d92e4f8a
Claudio Canales <klaudioz@gmail.com>
2020-04-16 15:21:01 -0600

    Fix jq parsing

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
-sample='[{"name":"foo"},{"name":"bar"}]'
-for row in $(echo "${sample}" | jq -c '.[]'); do
+# Removing spaces, slashs and newlines
+labels_json=$(echo "${1}" | sed 's/\\//g' | tr -d '\n' |tr -d ' ')
+
+for row in $(echo "${labels_json}" | jq -c '.[]'); do
     _jq() {
      echo ${row} | jq -c ${1}
     }
-
    echo $(_jq '.name')
 done
+
+
