a7a58c8b77591c608ef66d56e8311bced818e486
Claudio Canales <klaudioz@gmail.com>
2020-05-21 16:47:52 -0600

    Update pr_tests.sh

--- images/pr_tests.sh
+++ images/pr_tests.sh
@@ -13,7 +13,7 @@
 labels_json=$(echo "${1}" | sed 's/\\//g' | tr -d '\n' |tr -d ' ')
 
 # Variables
-RESOURCE_ID="124159" # I will need to update this ID when the new shippable job is created
+RESOURCE_ID="126842" # I will need to update this ID when the new shippable job is created
 
 # Storing the name of the labels to an array using jq
 for row in $(echo "${labels_json}" | jq -c '.[]'); do
@@ -23,6 +23,6 @@ done
 if [[ "${labels_str[@]}" =~ "e2e_gcloud" ]] || [[ "${labels_str[@]}" =~ "e2e_aws" ]] || [[ "${labels_str[@]}" =~ "e2e_do" ]] || [[ "${labels_str[@]}" =~ "e2e_azure" ]] || [[ "${labels_str[@]}" =~ "e2e_eks" ]]; then
     CLOUD=$(printf '%s\n' ${labels_str[@]} | grep 'e2e' | head -1 | cut -d "_" -f2)
     if [ -n "$CLOUD" ]; then
-        curl -sX POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: apiToken ${shippable_token}" -d '{"globalEnv": {"test_cloud": "$CLOUD"}}' "https://api.shippable.com/resources/${RESOURCE_ID}/triggerNewBuildRequest"
+        curl -v -sX POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: apiToken ${shippable_token}" -d '{"globalEnv": {"test_cloud": "$CLOUD"}}' "https://api.shippable.com/resources/${RESOURCE_ID}/triggerNewBuildRequest"
     fi
 fi
